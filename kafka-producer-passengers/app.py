79588ea42411d07209567219c20526a2d043b094
tricarico672 <tricarico672@gmail.com>
2025-05-04 19:36:32 +0200

    - fixed api, now predictions are made correctly based on time, route, and stop

--- kafka-producer-passengers/app.py
+++ kafka-producer-passengers/app.py
@@ -30,18 +30,19 @@ def create_kafka_producer():
 # Use it in your main code
 producer = create_kafka_producer()
 
-TIME_MULTIPLIER = 300
-real_start = time.time()
+TIME_MULTIPLIER = 10e3 # about every 15 min
+
 app_start = datetime(2025, 1, 1, 6, 0, 0)
 
-def get_simulated_time():
+def get_simulated_time(real_start = time.time()):
     elapsed_real = time.time() - real_start
     elapsed_simulated = timedelta(seconds=elapsed_real * TIME_MULTIPLIER)
     return app_start + elapsed_simulated
 
 def get_passengers(stop, route):
+    real_start = time.time()
     while True:
-        sim_time = get_simulated_time()
+        sim_time = get_simulated_time(real_start=real_start)
         seconds = (sim_time - sim_time.replace(hour=0, minute=0, second=0)).total_seconds()
         data_x = pd.DataFrame({
             'arrival_time': [seconds],
@@ -53,7 +54,7 @@ def get_passengers(stop, route):
         hour_start = 6 # the time when bus starts service
 
         if seconds > 3600 * hour_stop and seconds < 3600 * hour_start:
-            passenger_num = 0
+            break # passenger_num = 0
         else:
             passenger_num = int(model.predict(data_x)[0])
 
@@ -65,6 +66,15 @@ def get_passengers(stop, route):
         }
         print("Sending:", payload)
         producer.send('bus.passenger.predictions', value=payload)
-        time.sleep(2)
+        time.sleep(0.1)
 
-get_passengers(2278, '5')
+with open("route_to_stops.json", "r") as f:
+    route_stop_map = json.load(f)
+
+def main():
+    for route in route_stop_map:
+        for stop in route_stop_map[route]['stops']:
+            get_passengers(stop, route)
+
+if __name__ == "__main__":
+    main()
